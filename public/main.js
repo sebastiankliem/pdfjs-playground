b076c4bdea9846c6f46cfded5184fcb8fc081ef2
Kliem, Sebastian <sebastian.kliem@sap.com>
2017-10-26 12:04:00 +0200

    initial commit. basic page with file input.

---
+++ public/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#btn-load').addEventListener('click', function(e) {
+        e.preventDefault();
+        
+    }, false);
+});
