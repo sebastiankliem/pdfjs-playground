3e2752cc9559272074122bb6d24e196d0c73f101
Kliem, Sebastian <sebastian.kliem@sap.com>
2017-10-26 12:18:30 +0200

    load pdf without displaying it

--- public/main.js
+++ public/main.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('#btn-load').addEventListener('click', function(e) {
         e.preventDefault();
-        
+        displayPDF(getPDFUrl());
     }, false);
 });
+
+function getPDFUrl() {
+    return document.querySelector('#input-pdf').value;
+};
+
+function setupPDFjs() {
+    PDFJS.workerSrc = 'node_modules/pdfjs-dist/build/pdf.worker.js';
+}
+
+function displayPDF(url) {
+    console.log('display pdf');
+    PDFJS.getDocument(url).then(function(pdf) {
+        console.log(pdf);
+    });
+}
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use('/node_modules', express.static('node_modules'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
