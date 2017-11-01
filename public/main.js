7200814c305690b0484cfa364f96786a92ad2eb8
Kliem, Sebastian <sebastian.kliem@sap.com>
2017-11-01 11:06:22 +0100

    use PageViewer and TextLayer

--- public/main.js
+++ public/main.js
@@ -27,24 +27,27 @@ function displayPDF(url) {
     
     PDFJS.getDocument(url).then(function(pdf) {
         for(let i = 1; i <= pdf.numPages; i++) {
-            pdf.getPage(i).then(page => renderPage(page));
+            pdf.getPage(i).then(page => renderPage(i, page, viewer));
         }
     });
 }
 
-function renderPage(page) {
-    var scale = 1.5;
+function renderPage(pageNumber, page, viewer) {
+    var scale = 1;
     var viewport = page.getViewport(scale);
 
-    var canvas = document.createElement('canvas');
-    document.querySelector('#pdf-viewer').appendChild(canvas);
-    var context = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    var renderContext = {
-        canvasContext: context,
-        viewport: viewport
-    };
-    page.render(renderContext);
+    
+    // Creating the page view with default parameters.
+    var pdfPageView = new PDFJS.PDFPageView({
+        container: viewer,
+        id: pageNumber,
+        scale: scale,
+        defaultViewport: page.getViewport(scale),
+        // We can enable text/annotations layers, if needed
+        textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
+        annotationLayerFactory: new PDFJS.DefaultAnnotationLayerFactory()
+    });
+    // Associates the actual page with the view, and drawing it
+    pdfPageView.setPdfPage(page);
+    return pdfPageView.draw();
 }
